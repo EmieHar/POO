597f5d9d9e3f9b7b4bbadd6d2067b826bfdc4203
EmieHarit <emilie.haritchabalet@hotmail.fr>
2023-01-13 11:37:52 +0100

    version fin cours2

--- upload/Upload.php
+++ upload/Upload.php
@@ -4,11 +4,20 @@ echo "<pre>";
 print_r ($_FILES);
 echo "</pre>";
 
+$ext = pathinfo($_FILES['fichier']['name'], PATHINFO_EXTENSION);
+$extensions = ['.png', '.jpg', '.gif', '.jpeg'];
+
 $dossier = "Telechargements/";
 $fichier = $_FILES['fichier']['name'];
 
+if (in_array($ext, $extensions)) {
+
 if(move_uploaded_file($_FILES['fichier']['tmp_name'],$dossier.$fichier)){
     echo "téléversement de votre fichier réussi";
+}echo "erreur de téléversement";
+
+}else{
+    echo "extension de fichier non supporté";
 }
 
 ?>
